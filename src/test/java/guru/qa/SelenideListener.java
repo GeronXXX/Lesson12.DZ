7ea54ca9b5051962f8a7cfd6b4ccf8358a88d96d
GeronXXX <lityagin.ru@gmail.com>
2023-04-04 21:10:15 +0300

    v2

--- src/test/java/guru/qa/SelenideListener.java
+++ src/test/java/guru/qa/SelenideListener.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -12,16 +13,19 @@ import static com.codeborne.selenide.Selenide.open;
 
 
 public class SelenideListener {
-
+    @DisplayName("Тест - 1. Чистый Selenide (с Listener)")
     @Test
     public void testSelenideListener() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         open("https://github.com/");
         $(".header-search-input").setValue("GeronXXX/Lesson10.Files");
+        $(".header-search-input").pressEnter() ;
+        $(".f4.text-normal").click();
+        $("#issues-tab").click();
     }
-
     @AfterAll
     static void after() {
         Configuration.holdBrowserOpen = true;
     }
+
 }
